1b7a7f642576072639a4f9804d11e97b98a45e2a
Patrick Jahr <patrick.jahr@thinktecture.com>
2020-11-16 06:46:11 +0100

    update ui for mobile

--- frontend/angular-signalr-sample/src/environments/environment.ts
+++ frontend/angular-signalr-sample/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  identityBaseUrl: 'http://192.168.2.100.:5000',
-  apiBaseUrl: 'http://192.168.2.100:5002/'
+  identityBaseUrl: 'https://pj-tt-idsrv.azurewebsites.net',
+  apiBaseUrl: 'http://localhost:5002/'
 };
 
 /*
