996ef6f6a5e442d47f510e63cdc4b9fb7f1f745a
Patrick Jahr <patrick.jahr@thinktecture.com>
2020-11-17 22:34:10 +0100

    prepare webinar

--- backend/SignalRSample.Api/Hubs/GamesHub.cs
+++ backend/SignalRSample.Api/Hubs/GamesHub.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+using SignalRSample.Api.Models;
+
 namespace SignalRSample.Api.Hubs
 {
     public class GamesHub
     {
+        private List<GameSession> _sessions = new List<GameSession>();
+
+        public GamesHub()
+        {
+        }
     }
 }
--- backend/SignalRSample.Api/Startup.cs
+++ backend/SignalRSample.Api/Startup.cs
@@ -24,14 +24,14 @@ namespace SignalRSample.Api
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<GamesDbContext>(
                 options => options.UseInMemoryDatabase("TicTacToe"));
-            services.AddScoped<GameSessionManager>();
+            services.AddSingleton<GameSessionManager>();
             services.AddScoped<IUsersService, UsersService>();
             services.AddControllers();
             services.AddCors(options =>
@@ -83,8 +83,6 @@ namespace SignalRSample.Api
                         }
                     };
                 });
-
-            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
